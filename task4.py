44d075f381834e1f4319c76e1cc0f8c641ed4b09
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2024-09-29 15:43:47 +0700

    Update task4.py

---
+++ task4.py
@@ -0,0 +1,8 @@
+print("Напишите возраст")
+a=int(input())
+if a%10==1 and (a%100<5 or a%100>20):
+    print(a, "год");
+elif (a%10==2 or a%10==3 or a%10==4) and (a%100<5 or a%100>20):
+    print(a, "года");
+else:
+    print(a, "лет");
