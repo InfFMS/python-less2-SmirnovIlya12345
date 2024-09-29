357272732e5fc87cf4fa990ecfba2796726da758
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2024-09-29 15:42:50 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Write the lesson's number")
+a=int(input())
+b=455+a*55
+c=b//60
+d=b%60
+e=500+a*55
+f=e//60
+g=e%60
+print("The lesson starts at - ", c, ":", d)
+print("The lesson ends at - ", f, ":", g)
