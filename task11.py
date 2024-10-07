cfdb4f2ce8e3afeb7c0098b96fc961fb58f07eba
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2024-10-07 14:07:50 +0700

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,14 +1,14 @@
 print("Write an integer")
 a1=float(input())
 print("Write an integer")
-a2=float(input())
-print("Write an integer")
 b1=float(input())
+print("Write an integer")
+a2=float(input())
 print("I think you got the point")
 b2=float(input())
-if a2<b1:
+if a2>b1:
     print(":(");
 if a2==b1:
     print(a2);
-if a2>b1:
-    print("[", b1, ";", a2, "]")
+if a2<b1:
+    print("[", a2, ";", b1, "]")
