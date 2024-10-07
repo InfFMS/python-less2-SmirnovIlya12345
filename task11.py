d8e4520af7931f86f04aee59cbd5183f34181fda
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2024-10-07 22:19:22 +0700

    Пожалуйста, проверьте задание 11.py

--- task11.py
+++ task11.py
@@ -7,8 +7,9 @@ a2=float(input())
 print("I think you got the point")
 b2=float(input())
 if a2>b1:
-    print(":(");
-if a2==b1:
-    print(a2);
-if a2<b1:
+    print(":(")
+elif a2==b1:
+    print(a2)
+elif a2<b1:
     print("[", a2, ";", b1, "]")
+# Пожалуйста, проверьте задание ещё раз. Я его исправил.
